7738d4bb7b9feccf92ed9dcdd662d6b93ca87a83
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-01-09 15:30:33 +0100

    Correct modulo

--- factors.py
+++ factors.py
@@ -14,20 +14,20 @@ a2 = 0
 b2 = 1
 c2 = 0
 
-for i in range(0, 100000):
+for i in range(0, 32537631):
     # a = (3 * A[-1] + sum(A)) % 0x3FFFFFFF
     a1 = int(5 * A1[-1] - 3 * A1[-2]) % 0x3FFFFFFF
-    a2 = a1 + a2 % 0x3FFFFFFF
+    a2 = (a1 + a2) % 0x3FFFFFFF
     A1.append(a1)
 
     # b = (3 * B[-1] + sum(B) + 1) % 0x3FFFFFFF
     b1 = int(5 * B1[-1] - 3 * B1[-2]) % 0x3FFFFFFF
-    b2 = b1 + b2 % 0x3FFFFFFF
+    b2 = (b1 + b2) % 0x3FFFFFFF
     B1.append(b1)
 
     # c = (3 * C[-1] + sum(C) + 33 * i) % 0x3FFFFFFF
     c1 = (5 * C1[-1] - 3 * C1[-2] + 33) % 0x3FFFFFFF
-    c2 = c1 + c2 + 33 % 0x3FFFFFFF
+    c2 = (c1 + c2 + 33) % 0x3FFFFFFF
     C1.append(c1)
 
     ai1 = (a1 * seed1 + b1 * seed2 + c1) % 0x3FFFFFFF
